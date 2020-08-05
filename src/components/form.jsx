60ce378216a1155b8a235324943426e47f6b892b
Hugo Veloso <hugopcveloso@gmail.com>
2020-08-05 23:48:12 +0100

    changed form description

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -18,21 +18,26 @@ export default class MyForm extends React.Component {
 						{' '}
 						Estou actualmente aberto a trabalhar em novos projectos, sempre procurei ser versátil no desenvolvimento de
 						projectos, pois acredito que na maioria das vezes não é necessário uma grande equipa para a criação de um
-						site/projecto de qualidade contacta-me abaixo com o que necessitas e eu quando puder envio-te o orçamento
-						que ficaria muito mais barato quando comparando com uma consultora. Caso o projecto seja de grande dimensão
-						tenho também colegas nas mais diversas áreas que faço parcerias (desde Direito Empresarial, a Ilustração e
-						Moda)
+						site/projecto de qualidade. <br /> A vantagem de trabalhar comigo não é apenas o preço (muito mais barato do
+						que uma consultora), mas é principalmente a comunicação directa e o entendimento transversal de design,
+						desenvolvimento e marketing que resultam num trabalho mais coeso com os objectivos definidos pelo cliente.{' '}
+						<br /> Se precisares de algum serviço contacta-me abaixo, mesmo que o projecto seja de grande dimensão ou
+						necessidades diferentes tenho colegas nas mais diversas áreas com que já fiz parcerias (desde Direito
+						Empresarial, Programação, Animação 3D, etc...).
 					</p>
 					<ul className="services">
-						<li> Desenvolvimento e manutenção de Landing Pages</li>
-						<li> Web Development de ideias complexas como um fórum</li>
+						<li> Design e Desenvolvimento e manutenção de sites simples </li>
+						<li> Web Development de plataformas mais complexas</li>
 						<li>
 							{' '}
-							Todo o tipo de serviços de programação (scraping, automação de certos serviços, debugging, etc...){' '}
+							Todo o tipo de serviços que envolvam programação (scraping, automação de serviços digitais repetitivos,
+							debugging do teu site, etc.){' '}
 						</li>
-						<li> Desenvolver uma estratégia de Marketing Digital para a tua marca</li>
-						<li> Digitalizar uma loja física (de landing page a ecommerce) </li>
-						<li> Design para Redes Sociais </li>
+						<li> Desenvolver uma estratégia de Marketing Digital para a tua empresa/marca</li>
+						<li> Criar uma loja online </li>
+						<li> Criação de conteúdo para Redes Sociais (Design e Vídeo) </li>
+						<li> Digitalizar o teu negócio (loja online, serviço entregas, eventos, etc...)</li>
+						<li>etc. </li>
 					</ul>
 				</div>
 
@@ -41,12 +46,12 @@ export default class MyForm extends React.Component {
 					<br />
 					<input className="form-email" type="email" name="email" />
 					<br />
-					<label className="form-labels">Messagem:</label>
+					<label className="form-labels">Mensagem:</label>
 					<br />
 					<textarea className="form-message" type="text" name="message" rows="5" />
 					<br />
-					{status === 'SUCCESS' ? <p>Thanks!</p> : <button className="form-button">Submit</button>}
-					{status === 'ERROR' && <p>Ooops! There was an error.</p>}
+					{status === 'SUCCESS' ? <p>Obrigado!</p> : <button className="form-button">Enviar</button>}
+					{status === 'ERROR' && <p>Houve um erro na submissão, tenta dar refresh na página e tentar outra vez</p>}
 				</form>
 			</div>
 		);
