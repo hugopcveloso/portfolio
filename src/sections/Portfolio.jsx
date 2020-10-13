fd4eff420cd483089e1453eda23c3847b1b7eea8
hugopcveloso <hugopcveloso@gmail.com>
2020-10-13 20:16:46 +0100

    alignment fix

--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -3,14 +3,14 @@ import WorkList from '../components/work-list'
 
 const Portfolio = () => {
   return (
-    <div>
+
       	<div className="section-container" id="portfolio-section" >
 					<h2 className="portfolio-title"> My Work </h2>
 					<div id="work-list-container">
 						<WorkList />
 					</div>
 				</div>
-    </div>
+
   )
 }
 
